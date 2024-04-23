84e33c0fa70fee15dd51aea43b6c59d90a8a97bd
David Premkumar <138401271+davidpk-cs@users.noreply.github.com>
2024-04-22 22:28:14 -0400

    Cleaned up game code

--- Sprint3/Sprint2/Code/src/Space Defenders Demo/mainGame.js
+++ Sprint3/Sprint2/Code/src/Space Defenders Demo/mainGame.js
@@ -1,22 +1,21 @@
-var enemies = [];
-var enemyFragments = [];
-var enemyHP = [];
+var enemies = []; //enemy objects
+var enemyFragments = []; //this array stores enemy "speeds"
+var enemyHP = []; //enemy hitpoints
 
-var towers = [];
+var towers = []; //tower objects
 
-var enemyCount = 3;
-var enemyWaves = -1;
+var enemyCount = 3; //number of enemies per wave
+var enemyWaves = -1; //number of waves of enemies sent in
 
-var enemySpeed = 10;
+var lasersActive = false; //indicating that lasers are present on the screen, so we know to clean
 
-var lasersActive = false;
+var score = 0; //number of points
 
-var score = 0;
+var credits = 10; //money
 
-var credits = 10;
+var lives = 100; //lives
 
-var lives = 100;
-
+//------------
 const framesPerSecond = 60; //program tested on high refresh rate screens it works fine
 
 const totalTimeInSeconds = 0.1; // Total time for one cycle
@@ -27,32 +26,42 @@ const totalFrames = framesPerSecond * totalTimeInSeconds;
 const functionFrames = framesPerSecond * functionTimeInSeconds;
 var currentFunction = 0; //so we know what func to run
 
-var canPlace = true;
-var activeTowerPlacement = false;
-
-var assetsPath = "../assets/"
+var currentFrame = 0; //current frame we are on, for tracking frames.
+//--------------
 
 
-var currentFrame = 0;
+var canPlace = true; 
+var activeTowerPlacement = false; 
+// ^ when true that means the user hit the place tower button and hasn't placed yet
+
+
+var assetsPath = "../assets/" //location of assets folder
+
+
+
 
 function moveEnemies() {
     //console.log("First function");
 
     for (var i = 0; i < enemies.length; i++) {
+
+        //get the target object
         var target = document.getElementById("homeBase");
     
+        //get styles of the thing that is being targetted and enemy
         var targetStyles = window.getComputedStyle(target);
         var enemyStyles = window.getComputedStyle(enemies[i]);
     
+        // get y value
         var targetTop = parseFloat(targetStyles.top);
         var targetLeft = parseFloat(targetStyles.left);
     
+        //get x value
         var enemyTop = parseFloat(enemyStyles.top);
         var enemyLeft = parseFloat(enemyStyles.left);
     
-        //console.log(targetTop, targetLeft, enemyTop, enemyLeft);
     
-        // Calculate 1/1000th of the distance
+        // Calculate the fraction of the distance to move in this frame
         var changeX = (targetLeft - enemyLeft) / enemyFragments[i];
         var changeY = (targetTop - enemyTop) / enemyFragments[i];
     
@@ -64,27 +73,30 @@ function moveEnemies() {
         enemies[i].style.left = `${newX}px`;
         enemies[i].style.top = `${newY}px`;
 
+        //by reducing speed fragment we make it speed up the next time
+        //this is to make sure that enemies aren't slowing down
         if(enemyFragments[i] > 5){
             enemyFragments[i] -= 1;
         }
         
         var contactThreshold = 10; // pixels you need to be close enough to count as a touch
         if (Math.abs(newX - targetLeft) <= contactThreshold && Math.abs(newY - targetTop) <= contactThreshold) {
-            lives--;
+            
+            lives--; //enemy hit target, delete enemy
 
-            if(lives == 0){
+            attackEnemy(i, true);
+
+
+            if(lives == 0){ //force end game upon end of game
                 document.getElementById("livesBoard").innerText = "Lives Remaining: 0";
                 return;
             }
             
         }
 
-        if (enemyFragments[i] > 5) {
-            enemyFragments[i] -= 1;
-        }
-
     }
 
+    //update lives count
     document.getElementById("livesBoard").innerText = "Lives Remaining: " + lives.toString();
 }
 
@@ -99,52 +111,45 @@ function activateTowers() {
 
     for(var i = 0; i < towers.length; i++){
 
+        //tower styles
         var towerStyles = window.getComputedStyle(towers[i])
         var towerTop = parseInt(towerStyles.top);
         var towerLeft = parseInt(towerStyles.left);
 
         // Set the drawing buffer size to match the displayed size so we can draw on full canvas
-
         var towerRect = towers[i].getBoundingClientRect();
 
+        //tower dimensions
         const towerX = towerRect.left - canvasRect.left;
         const towerY = towerRect.top - canvasRect.top
 
-        console.log(towerX, towerY);
-
-    
         for(var j = 0; j < enemies.length; j++){
             
-            
+            //enemy styles
             var enemyStyles = window.getComputedStyle(enemies[j]);
             var enemyTop = parseInt(enemyStyles.top);
             var enemyLeft = parseInt(enemyStyles.left);
             
+            //enemy dimensions
             var enemyRect = enemies[j].getBoundingClientRect();
             const enemyX = enemyRect.left - canvasRect.left;
             const enemyY = enemyRect.top - canvasRect.top;
             
-
+            //calculate distance
             var distance = Math.sqrt((enemyTop - towerTop)**2 + (enemyLeft - towerLeft)**2);
 
-            //console.log(distance);
+            //if close enough, attack!
             if(distance < 100){
-                //console.log("found");
 
                 draw = canvas.getContext("2d");
-
                 draw.beginPath();
-                
                 draw.moveTo(towerX, towerY);
                 draw.lineTo(enemyX, enemyY);
-
                 draw.strokeStyle = "red";
-
                 draw.stroke();
-
                 attackEnemy(j);
                 
-                break;
+                break; //towers only attack one enemy a frame
             }
             else{
                 //console.log("not found");
@@ -152,15 +157,18 @@ function activateTowers() {
 
         }
 
+        //update score
         document.getElementById("scoreBoard").innerText = "Score: " + score.toString();
     }
 }
 
-function attackEnemy(j){
+//j the index of array
+//destroy is a parameter to indicate kill enemy even if it has sufficient hp
+function attackEnemy(j, destroy=false){
 
     enemyHP[j] -= 1;
 
-    if(enemyHP[j] == 0){
+    if(enemyHP[j] == 0 || destroy){
 
         enemies[j].parentNode.removeChild(enemies[j]);
         enemies.splice(j, 1);
@@ -174,10 +182,11 @@ function attackEnemy(j){
 
 function runRound() {
 
+    //lock the player from pressing buttons
     coverScreenWithTransparentDiv();
 
+    //the game alternates between 4 frames
     if (currentFrame < functionFrames) {
-
 
         if (currentFunction === 0) {
 
@@ -187,20 +196,27 @@ function runRound() {
             ctx.clearRect(0, 0, canvas.width, canvas.height);
             
             moveEnemies();
-        } else {
+        } 
+        
+        //frame 2
+        else {
             activateTowers();
         }
-    } else if (currentFrame < 2 * functionFrames) {
+    } 
+    
+    else if (currentFrame < 2 * functionFrames) {
+
         if (currentFunction === 1) {
 
-            if(enemyWaves){
+            if(enemyWaves && enemies.length < 40){
                 sendEnemyWave();
                 enemyCount++;
                 enemyWaves--;
             }
 
             moveEnemies();
-        } else {
+        } 
+        else {
             //nothing over here, just keep existing canvas
         }
     }
