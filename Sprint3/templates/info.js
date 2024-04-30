750a4ae0d8e939d3000e8e3a1356a9c6bc187115
k-rippy <128003273+k-rippy@users.noreply.github.com>
2024-04-30 17:19:43 -0400

    Update info.js

--- Sprint3/templates/info.js
+++ Sprint3/templates/info.js
@@ -4,14 +4,14 @@ conveyer_belt = {}
 shop = {}
 
 // RAW MATERIALS
-steel = {}
-copper = {}
+Steel = {}
+Copper = {}
 
 // INTERMEDIATE RESOURCES
 Plating_Recipe=        {Steel: 0                                               }
 Wire_Recipe=           {Copper: 0                                              }
 Battery_Recipe=        {Wire: 0,       Copper: 0                               }
-Motor_Recipe =         {Steel: 0,      Circuit: 0,         Battery: 0          }
+Motor_Recipe=          {Steel: 0,      Circuit: 0,         Battery: 0          }
 Circuit_Recipe=        {Plating: 0,    Wire: 0                                 }
 Laser_Optics_Recipe=   {Copper: 0,     Battery: 0                              }
 Engine_Recipe=         {Steel: 0,      Motor: 0                                }
@@ -24,3 +24,9 @@ Laser_Weapon_Recipe=   {Battery: 0,    Steel: 0,           Laser_Optics: 0     }
 Gunnery_Ship_Recipe=   {Engine: 0,     Plating: 0,         Gunnery_Weapon: 0   }
 Cannon_Ship_Recipe=    {Engine: 0,     Plating: 0,         Cannon_Weapon: 0    }
 Laser_Ship_Recipe=     {Engine: 0,     Plating: 0,         Laser_Weapon: 0     }
+
+Resources = [Steel, Copper, Plating_Recipe, Wire_Recipe, Battery_Recipe, Motor_Recipe, Circuit_Recipe, Laser_Optics_Recipe, 
+             Engine_Recipe, Battery_Recipe, Gunnery_Weapon_Recipe, Cannon_Weapon_Recipe, Laser_Weapon_Recipe]
+
+Recipes = [Plating_Recipe, Wire_Recipe, Battery_Recipe, Motor_Recipe, Circuit_Recipe, Laser_Optics_Recipe, Engine_Recipe, Battery_Recipe, 
+           Gunnery_Weapon_Recipe, Cannon_Weapon_Recipe, Laser_Weapon_Recipe, Gunnery_Ship_Recipe, Cannon_Ship_Recipe, Laser_Ship_Recipe]
